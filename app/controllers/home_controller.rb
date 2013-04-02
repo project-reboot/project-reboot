f0478d1e9a022e781a31ff5322cc2e8214153b09
Eno Compton 4 <eno4@ecom.com>
2013-04-02 11:52:00 -0700

    Update controller action for geocoder

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,9 @@ class HomeController < ApplicationController
   end
 
   def determine_ip_address
+    location_data = Geocoder.search('50.150.99.172') # TODO: change IP to request.remote_ip
     respond_to do |format|
-      format.json { render :json => Geocoder.search('50.150.99.172') }
+      format.json { render :json => location_data }
     end
   end
 end

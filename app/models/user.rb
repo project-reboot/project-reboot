9227f33a6c891b4658edde1e26f0a267802a38af
Elliot Shiu <elliot@sandbochs.com>
2013-05-19 21:50:25 -0700

    Add geocode support for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :knows_cpr, :street, :city, :state
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :knows_cpr
+  after_validation :geocode
+
+  geocoded_by :address
+
+  def address
+    [street, city, state].compact.join(', ')
+  end
 end
